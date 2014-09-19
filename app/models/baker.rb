0c2464e40d4b8811b559ae6b50c91a6dfb309484
Jesse Whitham <jesse@auraredeye.com>
2014-09-19 15:59:45 +1200

    Implement Baker Whirl
    * Initialize Bakers with a whirl priority
    * Add baked_cake method to work out new priority
    
    * Add .idea folder to gitignore (Rubymine Specific)

--- app/models/baker.rb
+++ app/models/baker.rb
@@ -1,2 +1,22 @@
 class Baker < ActiveRecord::Base
+  has_one :whirl
+  after_create :add_to_whirl
+
+  def add_to_whirl
+    Whirl.create(priority: new_priority, baker: self)
+  end
+
+  def baked_cake
+    self.whirl.update(priority: new_priority)
+  end
+
+  def new_priority
+    max_priority = Whirl.maximum(:priority)
+    if max_priority.nil?
+      new_priority = 1
+    else
+      new_priority = max_priority + 1
+    end
+    new_priority
+  end
 end

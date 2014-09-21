03074b86258b27be80c9e42ef113b270717a4bd8
Henry Jenkins <henry@henryjenkins.name>
2014-09-21 23:03:46 +1200

    Added email template and gmail smtp settings

--- test/mailers/previews/baker_mailer_preview.rb
+++ test/mailers/previews/baker_mailer_preview.rb
@@ -1,4 +1,17 @@
 # Preview all emails at http://localhost:3000/rails/mailers/baker_mailer
 class BakerMailerPreview < ActionMailer::Preview
+    def welcome
+      BakerMailer.welcome(Baker.first)
+    end
 
+    def your_next
+      BakerMailer.welcome(Baker.first)
+    end
+
+    def dont_forget
+      BakerMailer.welcome(Baker.first)
+    end
+
+    def cake_time
+    end
 end
